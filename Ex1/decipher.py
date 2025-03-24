e8d00276218a1f07a2a6dd1b99ae73905abccf99
Adi Karif <adika3@mta.ac.il>
2025-03-24 17:00:24 +0200

    finish

--- Ex1/decipher.py
+++ Ex1/decipher.py
@@ -1,21 +1,47 @@
 import json
-import pickle
+import pickle as pkl
+import sys
 
 
 def decipher_phrase(phrase, lexicon_filename, abc_filename):
-    # todo: implement this function
+
     print(f'starting deciphering using {lexicon_filename} and {abc_filename}')
+
+    try:
+        with open(abc_filename, 'r') as abc_file, open(lexicon_filename, 'rb') as lex_file:
+            lexicon = pkl.load(lex_file)
+            abc_data = abc_file.read().split('\n')
+            phrase_encoded = phrase.split()
+
+            if not phrase_encoded:
+                return {"status": 0, "orig_phrase": '', "K": -1}
+
+            for k in range(0,len(abc_data)):
+                temp_sentence = []
+                for encoded_word in phrase_encoded:
+                    temp_word = "".join(map(lambda s: abc_data[(abc_data.index(s) - k) % len(abc_data)], encoded_word))
+                    if temp_word not in lexicon:
+                        break
+                    else:
+                        temp_sentence.append(temp_word)
+                if(len(temp_sentence) == len(phrase_encoded)):
+                    real_sentence = " ".join(temp_sentence)
+                    return {"status": 1, "orig_phrase": real_sentence, "K" : k}
+
+
+    except Exception as e:
+        sys.exit(f"An error has occurred: {e}")
 
     # todo: due to possible difference in file encodings between operating systems, make sure to add
     #  utf8 encoding type when opening a file, as an example: with open(<file name>, 'r', encoding='utf8') as fin
     #  python developers plan to make utf8 a default at 3.15 - https://peps.python.org/pep-0686/
 
-    result = {"status": 0, "orig_phrase": '', "K": -1}
+    result = {"status": -1, "orig_phrase": '', "K": -1}
     return result
 
 
 # todo: fill in your student ids
-students = {'id1': '000000000', 'id2': '000000000'}
+students = {'id1': '2114407030', 'id2': '208295576'}
 
 if __name__ == '__main__':
     with open('config-decipher.json', 'r') as json_file:
--- Ex1/rps_game.py
+++ Ex1/rps_game.py
@@ -1,21 +1,64 @@
 import json
+import sys
 
+def get_round_winner(line):
+    """
+    input:a line of a round in the game
+    return: winner name or tie according to the game rules
+    """
+
+    round_lst = line.split()
+    player1_choice = round_lst[1]
+    player2_choice = round_lst[3]
+
+    rps_rules = {"paper": "rock", "scissors": "paper", "rock": "scissors"}
+
+    if player1_choice == player2_choice:
+        return None
+    elif player1_choice == rps_rules[player2_choice]:
+        return round_lst[2]
+    else:
+        return round_lst[0]
 
 def game(results_filename):
-    # todo: implement this function
+
+    game_scores = dict()
+
+    try:
+        with open(results_filename, "r") as res_file:
+            res_file.readline()
+            num_of_winners = 0
+
+            for line in res_file:
+                winner_in_round = get_round_winner(line)
+                if(winner_in_round):
+                    game_scores[winner_in_round] = game_scores.get(winner_in_round, 0) + 1
+
+            if game_scores:
+                winner_in_game = max(game_scores, key=game_scores.get)
+                num_of_winners = sum(1 for score in game_scores.values() if score == game_scores[winner_in_game])
+
+            if num_of_winners == 1:
+                game_result = winner_in_game
+            else:
+                game_result = "tie"
+
+    except Exception as e:
+        sys.exit(f"An error has occurred: {e}")
+
+
     print(f'starting the game with {results_filename}')
 
     # todo: due to possible difference in file encodings between operating systems, you may need to add
     #  utf8 encoding type when opening a file, as an example: with open(<file name>, 'r', encoding='utf8') as fin
     #  python developers plan to make utf8 a default at 3.15 - https://peps.python.org/pep-0686/
 
-
-    winner = ''  # todo: assign player name or "tie"
+    winner = game_result  # todo: assign player name or "tie"
     return winner
 
 
 # todo: fill in your student ids
-students = {'id1': '000000000', 'id2': '000000000'}
+students = {'id1': '211440730', 'id2': '208295576'}
 
 if __name__ == '__main__':
     with open('config-rps.json', 'r') as json_file:
